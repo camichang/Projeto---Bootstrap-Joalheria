3f356bba355b816c312aabca8015a4f31025fbca
Camila <87495830+camichang@users.noreply.github.com>
2021-08-30 14:27:54 -0300

    Update scriptteste.js

--- Projeto Bootstrap/js/scriptteste.js
+++ Projeto Bootstrap/js/scriptteste.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     });
 
     $('.backtotop').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 400);
+        $('html, body').animate({ scrollTop: 0 }, 1200);
         return false;
     })
 
